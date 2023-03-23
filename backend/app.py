07698be68c34a751019473ae34dfd341ab68d17a
Jaden Mary O'Brien <jmo229@cornell.edu>
2023-03-23 18:00:43 -0400

    Began boolean search

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = " "
+MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "drinksdb"
 
@@ -32,25 +32,27 @@ CORS(app)
 
 def sql_search(drink):
     # query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
-    query_sql = f"""SELECT * FROM mytable where LOWER( drink_name ) LIKE '%%{drink.lower()}%%' limit 5"""
+    # query_sql = f"""SELECT * FROM mytable where LOWER( drink_name ) LIKE '%%{drink.lower()}%%' limit 5"""
+    query_sql = f"""SELECT * FROM mytable"""
     # keys = ["id","title","descr"]
     keys = ["FIELD1", "drink_name", "ingredients",
             "quantities", "instructions"]
     data = mysql_engine.query_selector(query_sql)
-    drinks_data = json.dumps([dict(zip(keys, i)) for i in data])
+    # drinks_data = json.dumps([dict(zip(keys, i)) for i in data])
+    drinks_data = [dict(zip(keys, i)) for i in data]
     likes = drink[0]
     dislikes = drink[1]
     recs = []
     for dislike in dislikes:
-        for i, drink in enumerate(drinks_data[drink_name])
-        if (dislike not in drinks_data[ingredients][i])
-        recs.append((drink, drinks_data[ingredients][i]))
+        for i, drink in enumerate(drinks_data["drink_name"]):
+            if (dislike not in drinks_data["ingredients"][i]):
+                recs.append((drink, drinks_data["ingredients"][i]))
     acc = []
     set_likes = set(likes)
     for rec in recs:
         ingredients = set(recs[1])
-        if (len(set_likes.union(ingredients)) > 0)
-        acc.append(rec)
+        if (len(set_likes.union(ingredients)) > 0):
+            acc.append(rec)
     return acc
 
 
@@ -59,10 +61,13 @@ def home():
     return render_template('base.html', title="sample html")
 
 
-@app.route("/drinks")
+@app.route("/episodes")
 def drinks_search():
-    text = request.args.get("drink_name")
-    return sql_search(text)
+    text = request.args.get("title")
+    likes_dislikes = text.split()
+    likes = likes_dislikes[0].split(',')
+    dislikes = likes_dislikes[1].split(',')
+    return sql_search((likes, dislikes))
 
 
-# app.run(debug=True)
+app.run(debug=True)
